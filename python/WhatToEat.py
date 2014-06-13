68958eed15c3c6132e7f91ea67ea5ef731ae5861
nianglao <nianglao@163.com>
2014-06-13 17:13:19 +0800

    Add a simple comment.

--- python/WhatToEat.py
+++ python/WhatToEat.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*- 
 #coding=utf-8
 
+'''
+To my beloved girl friend TongXin.
+'''
+
 import  random
 menu=['手抓饼', '煎饼', '教工米线', '土豆粉', '清真食堂', '烤肉饭', '教工凉皮', '南门米线', '教工点菜',
       '桂林米粉', '南门抄手', '南门成都美食', '北门砂锅', '新食堂二层', '鸭腿饭']
